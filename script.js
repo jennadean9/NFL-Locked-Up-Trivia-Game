de98a72a1e53211bae08e201736504f1dee8075d
Jenna Dean <jenna.dean9@gmail.com>
2019-10-22 14:34:05 -0400

    about to implement classes to add 'true' to right answers

--- script.js
+++ script.js
@@ -10,11 +10,16 @@ const questionsArray =
 "The same year he won a Superbowl ring with the New York Giants, he got arrested for having a gun with no permit, found out after shooting himself in the leg at a club. Big year for...",
 "This 49er has quite the history with the po-po, but he is most notably known for being arrested after allegedly assaulting his 70 year old father. Who may this incredibly moral driven man be?"
 ]
-const optionsArray = [["Ray Rice", "Plaxico Burress", "Ben Roethlesberger", "Sebastian Janikowski"], ["Bruce Miller", "Jerry Rice", "Joe Montana", "Aldon Smith"]]
+const optionsArray = ["Ray Rice", "Ben Roethlesberger", "Sebastian Janikowski",  "Jerry Rice", "Joe Montana", "Aldon Smith"]
 
+const correctAnswers = {
+    names: ["Plaxico Burress", "Bruce Miller"],
+    correct: true
+}
 
 playButton.addEventListener("click", (e) => {
     startTrivia10();
+    checkAnswer();
 })
 
 console.log(questionsArray)
@@ -29,12 +34,24 @@ function startTrivia10(){
         optionSeen[2].innerText = optionsArray[currentRound][2]
         optionSeen[3].innerText = optionsArray[currentRound][3]
 }
-function rightAnswers(){
-    optionsArray[0][1].classList.add("rightAnswer")
-    optionsArray[1][0].classList.add("rightAnswer")
-}
+optionsArray[0][1].classList.add("rightAnswer")
+optionsArray[1][0].classList.add("rightAnswer")
+
 function checkAnswer(){
-    
+    options[currentRound].addEventListener("click", (e)=>{
+        e.preventDefault()
+        if(options[currentRound].classList.contains("rightAnswer")){
+            answer.innerText = `AYYY TRUE!`
+            currentRound++
+            points++
+        }else{
+            answer.innerText = `NOPE! The correct answer was ${options[currentRound].classList.contains("rightAnswer")}`
+            currentRound++
+        }
+    })
+}
+
+
 // playButton.addEventListener("click", (e) => {
 //     e.preventDefault();
 //     question.innerText = "The same year he won a Superbowl ring with the New York Giants, he got arrested for having a gun with no permit, found out after shooting himself in the leg at a club. Big year for..."
@@ -47,15 +64,23 @@ function checkAnswer(){
 
 // for(let i = 0; i < options.length; i++){
 //     options[i].addEventListener("click", (e)=>{
-//         e.preventDefault();
-//         if (options[i] -xc[                                                                vxxxxc ].classList.contains("rightAnswer")){
-//             answer.innerText = `AYY TRUE!`
-//             currentRound++
-//             points++
-//         }
+        // e.preventDefault();
+        // if (options[i].classList.contains("rightAnswer")){
+        //     answer.innerText = `AYY TRUE!`
+        //     currentRound++
+        //     points++
+        // }
 //         else {
 //             answer.innerText = `NOPE! The correct answer was `
 //             currentRound++
 //         }
 //     })
 //}
+
+//trying it with classes
+// class Question{
+//     constructor(question, answer){
+//         this.question = question
+//         this.answer = answer
+//     }
+// }
