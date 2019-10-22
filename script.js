8ee2989dee55954a214a248b895c7052e90afc1f
Jenna Dean <jenna.dean9@gmail.com>
2019-10-22 11:53:27 -0400

    trivia format creating arrays

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ const answer = document.getElementById("answer");
 const nextButton = document.querySelector(".next");
 let currentRound = 0;
 let points = 0;
-
+const questionsArray = ["The same year he won a Superbowl ring with the New York Giants, he got arrested for having a gun with no permit, found out after shooting himself in the leg at a club. Big year for..."]
+const optionsArray = [
+    ["Ray Rice", "Plaxico Burress", "Ben Roethlesberger", "Sebastian Janikowski"]
+]
 playButton.addEventListener("click", (e) => {
     e.preventDefault();
-    question.innerText = "The same year he won a Superbowl ring with the New York Giants, he got arrested for having a gun with no permit, found out after shooting himself in the leg at a club. Big year for..."
-    options[0].innerText = "Ray Rice"
-    options[1].innerText = "Plaxico Burress"
+    question.innerText = questionsArray[0]
+    options[0].innerText = optionsArray[0][0]
+    options[1].innerText = optionsArray[0][1]
     options[1].classList.add("rightAnswer");
-    options[2].innerText = "Ben Roethlesberger"
-    options[3].innerText = "Sebastian Janikowski"
+    options[2].innerText = optionsArray[0][2]
+    options[3].innerText = optionsArray[0][3]
 })
 console.log(options)
 for(let i = 0; i < options.length; i++){
@@ -31,3 +34,13 @@ for(let i = 0; i < options.length; i++){
     })
 }
 
+function startTrivia10(e){
+    for (let i = 0; i < 10; i++){
+        e.preventDefault()
+        questionsArray.innerText = questionsarray[i]
+        options[0].innerText = optionsArray[i][0]
+        options[1].innerText = optionsArray[i][1]
+        options[1].innerText = optionsArray[i][2]
+        options[1].innerText = optionsArray[i][3]
+    }
+}
