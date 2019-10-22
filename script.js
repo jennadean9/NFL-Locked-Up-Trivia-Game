a4d71f8cefa1adf13f5b43111a9cbd54abc27f29
Jenna Dean <jenna.dean9@gmail.com>
2019-10-22 13:59:33 -0400

    things are happening

--- script.js
+++ script.js
@@ -10,22 +10,45 @@ const questionsArray =
 "The same year he won a Superbowl ring with the New York Giants, he got arrested for having a gun with no permit, found out after shooting himself in the leg at a club. Big year for...",
 "This 49er has quite the history with the po-po, but he is most notably known for being arrested after allegedly assaulting his 70 year old father. Who may this incredibly moral driven man be?"
 ]
-const optionsArray = [
-    ["Ray Rice", "Plaxico Burress", "Ben Roethlesberger", "Sebastian Janikowski"],
-    ["Bruce Miller", "Jerry Rice", "Joe Montana", "Aldon Smith"]
-]
+const optionsArray = [["Ray Rice", "Plaxico Burress", "Ben Roethlesberger", "Sebastian Janikowski"], ["Bruce Miller", "Jerry Rice", "Joe Montana", "Aldon Smith"]]
+
+
 playButton.addEventListener("click", (e) => {
     startTrivia10();
 })
 
 console.log(questionsArray)
 console.log(questionsArray[0])
-console.log(optionSeen[0])
+console.log(optionSeen)
 console.log(optionsArray)
+
+function startTrivia10(){
+        question.innerText = questionsArray[currentRound]
+        optionSeen[0].innerText = optionsArray[currentRound][0]
+        optionSeen[1].innerText = optionsArray[currentRound][1]
+        optionSeen[2].innerText = optionsArray[currentRound][2]
+        optionSeen[3].innerText = optionsArray[currentRound][3]
+}
+function rightAnswers(){
+    optionsArray[0][1].classList.add("rightAnswer")
+    optionsArray[1][0].classList.add("rightAnswer")
+}
+function checkAnswer(){
+    
+// playButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     question.innerText = "The same year he won a Superbowl ring with the New York Giants, he got arrested for having a gun with no permit, found out after shooting himself in the leg at a club. Big year for..."
+//     options[0].innerText = "Ray Rice"
+//     options[1].innerText = "Plaxico Burress"
+//     options[1].classList.add("rightAnswer");
+//     options[2].innerText = "Ben Roethlesberger"
+//     options[3].innerText = "Sebastian Janikowski"
+// })
+
 // for(let i = 0; i < options.length; i++){
 //     options[i].addEventListener("click", (e)=>{
 //         e.preventDefault();
-//         if (options[i].classList.contains("rightAnswer")){
+//         if (options[i] -xc[                                                                vxxxxc ].classList.contains("rightAnswer")){
 //             answer.innerText = `AYY TRUE!`
 //             currentRound++
 //             points++
@@ -35,27 +58,4 @@ console.log(optionsArray)
 //             currentRound++
 //         }
 //     })
-// }
-
-function startTrivia10(){
-    for (let i = 0; i < 10; i++){
-        question.innerText = questionsArray[i]
-        optionSeen[0].innerText = optionsArray[i][0]
-        optionSeen[1].innerText = optionsArray[i][1]
-        optionSeen[2].innerText = optionsArray[i][2]
-        optionSeen[3].innerText = optionsArray[i][3]
-    }
-
-    // for (let i = 0; i < options[i].length; i++){
-    //     options[i].addEventListener("click", (e) =>{
-    //         if(options[i].classList.contains("rightAnswer")){
-    //             answer.innerText = `YOU GOT IT!`
-    //             currentRound++
-    //             points++
-    //         }else{
-    //             answer.innerText = `Nope! The correct answer was ${options[i]}`
-    //             currentRound++
-    //         }
-    //     })
-    // }
-}
+//}
