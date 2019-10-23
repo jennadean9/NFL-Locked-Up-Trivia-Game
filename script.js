de476dc93df175b25d0bab339ecb6529a5f99665
Jenna Dean <jenna.dean9@gmail.com>
2019-10-23 11:21:51 -0400

    cant get correct answer for answer

--- script.js
+++ script.js
@@ -30,8 +30,37 @@ const allQuestions = [
     ["Wes Welker", "Onterrio Smith",  "Cedric Benson", "Fran Tarkenton"],
     "Onterrio Smith")
 ];
+
+const playButton = document.querySelector(".play");
+const container = document.querySelector(".container");
+let currentRound = 0;
+const optionsContainer = document.querySelectorAll(".optionsContainer")
+let currentQuestion = allQuestions[currentRound].question;
+// let currentOptionA = allQuestions[currentRound].options[0]
+// let currentOptionB = allQuestions[currentRound].options[1]
+// let currentOptionC = allQuestions[currentRound].options[2]
+// let currentOptionD = allQuestions[currentRound].options[3]
+
+playButton.addEventListener("click", (e) => {
+    container.innerText = currentQuestion
+    optionsContainer[0].innerText = allQuestions[currentRound].options[0]
+    optionsContainer[1].innerText = allQuestions[currentRound].options[1]
+    optionsContainer[2].innerText = allQuestions[currentRound].options[2]
+    optionsContainer[3].innerText = allQuestions[currentRound].options[3]
+})
+
+for (let i = 0; i < optionsContainer.length; i++){
+    optionsContainer[i].addEventListener("click", (e) => {
+        if(optionsContainer[i] = true){
+            alert("True")
+        } else {
+            alert("Wrong")
+        }
+    })
+}
+
+console.log(allQuestions[currentRound].options)
 console.log(allQuestions)
-console.log(this)
 
 
 
@@ -45,11 +74,6 @@ console.log(this)
 
 
 
-
-
-
-
-// const playButton = document.querySelector(".play");
 // var optionA = document.getElementById(optionA);
 // var optionB = document.getElementById(optionB);
 // var optionC = document.getElementById(optionC);
@@ -59,38 +83,7 @@ console.log(this)
 // const nextButton = document.querySelector(".next");
 // let currentRound = 0;
 // let points = 0;
-// const questionsArray = 
-// [
-// "After celebrating a little too hard the year he won a Super Bowl ring in 2008, this guy accidently SHOT HIMSELF (yes really shot himself) in his thigh while readjusting the gun in his pocket. OH, also the gun had no permit.",
-// "Which smooth operating Viking rented a boat Lake Minetonka for him and 17 other smooth operating teammates in 2005 with about 100 prostitutes, most of which were doin' their thang with the players in front of hired crew members.",
-// "After one too many bourbons and kicked out of one too many frat parties, which big 'football' guy and party fiend got charged with hitting and threating his ex-girlfriend in Dallas?",
-// "This receiver decided to take an illegal u-turn. Pretty mild, huh? Wait for it. Which ballsy boy decided that the best move was to actually bump the policewoman enough so that she fell over?",
-// "Which unortunate former RB for the Vikings got put in handcuffs at the airport for carrying dried urine and a Whizzinator? Hm? What's a Whizzinator, you ask? Why, it is a device that turns dried urine into liquid urine."
-// ]
-// const possibleOptions = [
-//     ["Lawrence Taylor", "Plaxico Burress", "Ben Roethlesberger", "Sebastian Janikowski"], 
-//     ["Fred Smoot",  "Daunte Culpepper", "Jermaine Wiggins", "Troy Williamson"],
-//     ["Demaryius Thomas", "Terell Suggs", "Michael Bennett", "Johnny Manziel"],
-//     ["Odell Beckham Jr.", "Shaun Rogers", "Randy Moss",  "JaMarcus Russell"],
-//     ["Wes Welker", "Onterrio Smith",  "Cedric Benson", "Fran Tarkenton"]
-// ]
-// console.log(optionA, optionB, optionC, optionD)
-// function addRightAnswerClass(){
-//     possibleOptions[0][1].classList.add("rightAnswer");
-//     possibleOptions[1][0].classList.add("rightAnswer");
-//     possibleOptions[2][3].classList.add("rightAnswer");
-//     possibleOptions[3][2].classList.add("rightAnswer");
-//     possibleOptions[4][1].classList.add("rightAnswer");
 
-// }
-// playButton.addEventListener("click", (e) => {
-//     question.innerText = questionsArray[currentRound]
-//     optionA.innerText = possibleOptions[currentRound][0]
-//     optionB.innerText = possibleOptions[currentRound][1]
-//     optionC.innerText = possibleOptions[currentRound][2]
-//     optionD.innerText = possibleOptions[currentRound][3]
-//     addRightAnswerClass();
-// })
 
 // // function startTrivia(){
 // //         question.innerText = questionsArray[currentRound]
