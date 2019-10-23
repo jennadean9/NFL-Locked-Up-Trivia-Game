a83fa88def86833005cf51174304862ac727f683
Jenna Dean <jenna.dean9@gmail.com>
2019-10-22 21:11:08 -0400

    adding questions

--- script.js
+++ script.js
@@ -5,37 +5,33 @@ const answer = document.getElementById("answer");
 const nextButton = document.querySelector(".next");
 let currentRound = 0;
 let points = 0;
+let currentOptions = []; //push new correct answer into correct array, push 3 of the false ones in same array
 const questionsArray = 
 [
-"The same year he won a Superbowl ring with the New York Giants, he got arrested for having a gun with no permit, found out after shooting himself in the leg at a club. Big year for...",
-"This 49er has quite the history with the po-po, but he is most notably known for being arrested after allegedly assaulting his 70 year old father. Who may this incredibly moral driven man be?"
+"After celebrating a little too hard the year he won a Super Bowl ring in 2008, this guy shot himself in his thigh... shot... himself... while readjusting a gun. OH, also the gun had no permit.",
+"Which smooth operating Viking rented a boat Lake Minetonka for him and 17 other smooth operating teammates in 2005 with about 100 prostitutes, most of which were doin' their thang with the players in front of hired crew members.",
+"After one too many bourbons and kicked out of one too many frat parties, which big 'football' guy and party fiend got charged with hitting and threating his ex-girlfriend in Dallas?",
+"This receiver decided to take an illegal u-turn. Pretty mild, huh? Wait for it. Which ballsy boy decided that the best move was to actually bump the policewoman enough so that she fell over?",
+"Which unortunate former RB for the Vikings got put in handcuffs at the airport for carrying dried urine and a Whizzinator? Hm? What's a Whizzinator, you ask? Why, it is a device that turns dried urine into liquid urine."
 ]
-const optionsArray = ["Ray Rice", "Ben Roethlesberger", "Sebastian Janikowski",  "Jerry Rice", "Joe Montana", "Aldon Smith"]
+const possibleOptions = [
+    ["Lawrence Taylor", "Plaxico Burress", "Ben Roethlesberger", "Sebastian Janikowski",], 
+    ["Fred Smoot", "Daunte Culpepper", "Jermaine Wiggins", "Troy Williamson"],
+    ["Demaryius Thomas", "Terell Suggs", "Michael Bennett", "Johnny Manziel"],
+    ["Odell Beckham Jr.", "Shaun Rogers", "Randy Moss", "JaMarcus Russell"],
+    ["Wes Welker", "Onterrio Smith", "Cedric Benson", "Fran Tarkenton"]
+]
 
-const correctAnswers = {
-    names: ["Plaxico Burress", "Bruce Miller"],
-    correct: true
-}
 
 playButton.addEventListener("click", (e) => {
     startTrivia10();
     checkAnswer();
 })
 
-console.log(questionsArray)
-console.log(questionsArray[0])
-console.log(optionSeen)
-console.log(optionsArray)
-
 function startTrivia10(){
         question.innerText = questionsArray[currentRound]
-        optionSeen[0].innerText = optionsArray[currentRound][0]
-        optionSeen[1].innerText = optionsArray[currentRound][1]
-        optionSeen[2].innerText = optionsArray[currentRound][2]
-        optionSeen[3].innerText = optionsArray[currentRound][3]
+        p
 }
-optionsArray[0][1].classList.add("rightAnswer")
-optionsArray[1][0].classList.add("rightAnswer")
 
 function checkAnswer(){
     options[currentRound].addEventListener("click", (e)=>{
@@ -64,23 +60,15 @@ function checkAnswer(){
 
 // for(let i = 0; i < options.length; i++){
 //     options[i].addEventListener("click", (e)=>{
-        // e.preventDefault();
-        // if (options[i].classList.contains("rightAnswer")){
-        //     answer.innerText = `AYY TRUE!`
-        //     currentRound++
-        //     points++
-        // }
+//         e.preventDefault();
+//         if (options[i].classList.contains("rightAnswer")){
+//             answer.innerText = `AYY TRUE!`
+//             currentRound++
+//             points++
+//         }
 //         else {
 //             answer.innerText = `NOPE! The correct answer was `
 //             currentRound++
 //         }
 //     })
-//}
-
-//trying it with classes
-// class Question{
-//     constructor(question, answer){
-//         this.question = question
-//         this.answer = answer
-//     }
 // }
