874b4c75cc8726f48f122dce23770189e8dee5e9
Jenna Dean <jenna.dean9@gmail.com>
2019-10-25 10:16:39 -0400

    pretty much finished product

--- script.js
+++ script.js
@@ -7,30 +7,37 @@ class QandA {
 }
 
 const allQuestions = [
-    new QandA("Which running back pulled a fire alarm of a Hampton Inn after losing his key to his Hampton Inn suite?",
+    new QandA("Which running back pulled a fire alarm after losing his key leaving himself locked out of his Hampton Inn suite?",
         ["Adrian Peterson", "Derrick Henry", "Mark Ingram", "Dion Lewis"],
         "Dion Lewis"),
-    new QandA("After celebrating a little too hard the year of his Superbowl win, which smart guy accidently shot himself in his thigh?",
+    new QandA("After celebrating a little too hard the year of his Superbowl win, which smart guy accidently shot himself in his thigh at a nightclub?",
         ["Lawrence Taylor", "Plaxico Burress", "Ben Roethlesberger", "Sebastian Janikowski"],
         "Plaxico Burress"),
     new QandA("Which former player got arrested for stealing bottles of beer from a deli?",
         ["Brett Favre", "Mike Vrabel", "Michael Vick", "Jason Garrett"],
         "Mike Vrabel"),
-    new QandA("Which smooth operating Viking rented a boat Lake Minetonka for him and 17 other teammatesin 2005 with about 100 prostitutes and made these 'transactions' in front of the boat crew members?",
+    new QandA("Which smooth operating Viking rented a boat on Lake Minetonka for him and 17 other teammates with about 100 prostitutes and made 'transactions' with the women in front of the boat crew members?",
         ["Fred Smoot", "Daunte Culpepper", "Jermaine Wiggins", "Troy Williamson"],
         "Fred Smoot"),
-    new QandA("After one too many bourbons and kicked out of one too many frat parties, which big 'football' guy and party fiend got charged with hitting and threating his ex-girlfriend in Dallas?",
+    new QandA("After one too many bourbons, which big 'football' guy and party fiend got charged with hitting and threating his ex-girlfriend in Dallas?",
         ["Demaryius Thomas", "Terell Suggs", "Michael Bennett", "Johnny Manziel"],
         "Johnny Manziel"),
-    new QandA("This receiver decided to take an illegal u-turn. Pretty mild, huh? Wait for it. Which ballsy boy decided that the best move was to actually bump the policewoman enough so that she fell over?",
+    new QandA("Which receiver took an illegal u-turn, and then bumped and knocked over the policewoman that got out of her car to stop him?",
         ["Odell Beckham Jr.", "Shaun Rogers", "Randy Moss", "JaMarcus Russell"],
         "Randy Moss"),
-    new QandA("Which unortunate former RB for the Vikings got put in handcuffs at the airport for carrying dried urine and a Whizzinator? Hm? What's a Whizzinator, you ask? Why, it is a device that turns dried urine into liquid urine.",
+    new QandA("Which unortunate former RB for the Vikings got put in handcuffs at the airport for carrying dried urine and a Whizzinator? What's a Whizzinator, you ask? Why, it is a device that turns dried urine into liquid urine.",
         ["Wes Welker", "Onterrio Smith", "Cedric Benson", "Fran Tarkenton"],
         "Onterrio Smith"),
     new QandA("Which former cornerback started throwing upper cuts to the jaw of the bouncer outside of the club who wouldn't let him in         until he put his pants above his knees?",
         ["Jason Sehorn", "Richard Sherman", "Deion Sanders", "Cedric Griffin"],
-        "Cedric Griffin")
+        "Cedric Griffin"),
+    new QandA("Which former DT has an extensive history with violent traffic incidents, including a reckless boating charge?",
+        ["Albert Haynesworth", "Warren Sapp", "Richard Seymour", "Justin Smith"],
+        "Albert Haynesworth"),
+    new QandA("Which former Cowboy's Lamborghini was found abandoned after hitting a pole, and then found and arrested while he was riding an electric scoooter?",
+        ["Dez Bryant", "Roger Staubach", "Terrance Williams", "Cole Beasley"],
+        "Terrance Williams")
+
 ];
 const body = document.querySelector("body")
 const header = document.querySelector("header")
@@ -44,6 +51,7 @@ const comments = document.querySelector(".comments")
 const tryAgain = document.querySelector(".try-again")
 const subtitle = document.querySelector("h2")
 const title = document.querySelector("h1")
+const results = document.querySelector(".results")
 let currentRound = 0;
 let currentQuestion = allQuestions[currentRound].question;
 let points = 0;
@@ -82,7 +90,7 @@ function chooseAnswer() {
 
             } else {
                 currentRound++
-                comments.innerText = "Incorrect"
+                comments.innerText = `Incorrect. It was ${allQuestions[currentRound - 1].answer}!`
                 nextButton.style.visibility = "visible"
             }
         })
@@ -109,15 +117,16 @@ function hitNext() {
 
 function endTrivia() {
     if (currentRound === allQuestions.length) {
-        container.innerText = `ALL DONE! You scored ${points} out of ${allQuestions.length}`
+        container.innerText = `Your sentence is done! You scored ${points} out of ${allQuestions.length}`
         // try to do this with a loop or .forEach()
         optionsContainer[0].innerText = " "
         optionsContainer[1].innerText = " "
         optionsContainer[2].innerText = " "
         optionsContainer[3].innerText = " "
-        comments.innerText = " "
+        comments.style.display = "none"
         tryAgain.style.visibility = "visible"
         nextButton.style.visibility = "hidden"
+
     }
 }
 
