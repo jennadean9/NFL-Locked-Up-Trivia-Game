b922e0db6fba9ac9e81aac6f4d80c73d6a66f878
Jenna Dean <jenna.dean9@gmail.com>
2019-10-24 20:25:24 -0400

    adding animations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class QandA {
-    constructor(question, options, answer){
+    constructor(question, options, answer) {
         this.question = question;
         this.options = options;
         this.answer = answer;
@@ -7,86 +7,112 @@ class QandA {
 }
 
 const allQuestions = [
-    new QandA("After celebrating a little too hard the year he won a Super Bowl ring in 2008, this guy accidently SHOT HIMSELF (yes really shot himself) in his thigh while readjusting the gun in his pocket. OH, also the gun had no permit.",
-    ["Lawrence Taylor", "Plaxico Burress", "Ben Roethlesberger", "Sebastian Janikowski"], 
-    "Plaxico Burress"),
-    new QandA("Which smooth operating Viking rented a boat Lake Minetonka for him and 17 other smooth operating teammates in 2005 with about 100 prostitutes, most of which were doin' their thang with the players in front of hired crew members.",
-    ["Fred Smoot",  "Daunte Culpepper", "Jermaine Wiggins", "Troy Williamson"],
-    "Fred Smoot"),
+    new QandA("Which running back pulled a fire alarm of a Hampton Inn after losing his key to his Hampton Inn suite?",
+        ["Adrian Peterson", "Derrick Henry", "Mark Ingram", "Dion Lewis"],
+        "Dion Lewis"),
+    new QandA("After celebrating a little too hard the year of his Superbowl win, which smart guy accidently shot himself in his thigh?",
+        ["Lawrence Taylor", "Plaxico Burress", "Ben Roethlesberger", "Sebastian Janikowski"],
+        "Plaxico Burress"),
+    new QandA("Which former player got arrested for stealing bottles of beer from a deli?",
+        ["Brett Favre", "Mike Vrabel", "Michael Vick", "Jason Garrett"],
+        "Mike Vrabel"),
+    new QandA("Which smooth operating Viking rented a boat Lake Minetonka for him and 17 other teammatesin 2005 with about 100 prostitutes and made these 'transactions' in front of the boat crew members?",
+        ["Fred Smoot", "Daunte Culpepper", "Jermaine Wiggins", "Troy Williamson"],
+        "Fred Smoot"),
     new QandA("After one too many bourbons and kicked out of one too many frat parties, which big 'football' guy and party fiend got charged with hitting and threating his ex-girlfriend in Dallas?",
-    ["Demaryius Thomas", "Terell Suggs", "Michael Bennett", "Johnny Manziel"],
-    "Johnny Manziel"),
+        ["Demaryius Thomas", "Terell Suggs", "Michael Bennett", "Johnny Manziel"],
+        "Johnny Manziel"),
     new QandA("This receiver decided to take an illegal u-turn. Pretty mild, huh? Wait for it. Which ballsy boy decided that the best move was to actually bump the policewoman enough so that she fell over?",
-    ["Odell Beckham Jr.", "Shaun Rogers", "Randy Moss",  "JaMarcus Russell"],
-    "Randy Moss"),
+        ["Odell Beckham Jr.", "Shaun Rogers", "Randy Moss", "JaMarcus Russell"],
+        "Randy Moss"),
     new QandA("Which unortunate former RB for the Vikings got put in handcuffs at the airport for carrying dried urine and a Whizzinator? Hm? What's a Whizzinator, you ask? Why, it is a device that turns dried urine into liquid urine.",
-    ["Wes Welker", "Onterrio Smith",  "Cedric Benson", "Fran Tarkenton"],
-    "Onterrio Smith")
+        ["Wes Welker", "Onterrio Smith", "Cedric Benson", "Fran Tarkenton"],
+        "Onterrio Smith")
 ];
-
+const body = document.querySelector("body")
+const header = document.querySelector("header")
+const main = document.querySelector("main")
 const playButton = document.querySelector(".play");
-const container = document.querySelector(".container");
+const container = document.querySelector(".question-container");
+const nextButton = document.querySelector(".next-button")
+const optionsContainer = document.querySelectorAll(".options")
+const comments = document.querySelector(".comments")
+const tryAgain = document.querySelector(".try-again")
+const subtitle = document.querySelector("h2")
 let currentRound = 0;
-const nextButton = document.querySelector(".nextButton")
-const optionsContainer = document.querySelectorAll(".optionsContainer")
 let currentQuestion = allQuestions[currentRound].question;
-let comments = document.querySelector(".comments")
 let points = 0;
-const tryAgain = document.querySelector(".tryAgain")
 
-playButton.addEventListener("click", (e) => {
-    container.innerText = allQuestions[currentRound].question
-    optionsContainer[0].innerText = allQuestions[currentRound].options[0]
-    optionsContainer[1].innerText = allQuestions[currentRound].options[1]
-    optionsContainer[2].innerText = allQuestions[currentRound].options[2]
-    optionsContainer[3].innerText = allQuestions[currentRound].options[3]
-})
+function startGame() {
+    playButton.addEventListener("click", (e) => {
+        container.innerText = allQuestions[currentRound].question
+        // try to do this with a loop or .forEach()
+        optionsContainer[0].innerText = allQuestions[currentRound].options[0]
+        optionsContainer[1].innerText = allQuestions[currentRound].options[1]
+        optionsContainer[2].innerText = allQuestions[currentRound].options[2]
+        optionsContainer[3].innerText = allQuestions[currentRound].options[3]
+        playButton.style.visibility = "hidden"
+        subtitle.style.display = "none"
+        main.style.display = "block"
 
-for (let i = 0; i < optionsContainer.length; i++){
-    optionsContainer[i].addEventListener("click", (e) => {
-        if(optionsContainer[i].innerText == allQuestions[currentRound].answer){
-            currentRound++
-            comments.innerText = "Correct" 
-            nextButton.style.visibility = "visible"
-            points++
-        } else {
-            currentRound++
-            comments.innerText = "Incorrect"
-            nextButton.style.visibility = "visible"
-        }
+    })
+
+}
+
+function chooseAnswer() {
+    for (let i = 0; i < optionsContainer.length; i++) {
+        optionsContainer[i].addEventListener("click", (e) => {
+            if (optionsContainer[i].innerText == allQuestions[currentRound].answer) {
+                currentRound++
+                comments.innerText = "Correct"
+                nextButton.style.visibility = "visible"
+                points++
+            } else {
+                currentRound++
+                comments.innerText = "Incorrect"
+                nextButton.style.visibility = "visible"
+            }
+        })
+    }
+}
+function hitNext() {
+    nextButton.addEventListener("click", (e) => {
+        endTrivia()
+        nextButton.style.visibility = "hidden"
+        comments.innerText = " "
+        container.innerText = allQuestions[currentRound].question
+        // try to do this with a loop or .forEach() 
+        optionsContainer[0].innerText = allQuestions[currentRound].options[0]
+        optionsContainer[1].innerText = allQuestions[currentRound].options[1]
+        optionsContainer[2].innerText = allQuestions[currentRound].options[2]
+        optionsContainer[3].innerText = allQuestions[currentRound].options[3]
+        console.log(currentRound)
     })
 }
-nextButton.addEventListener("click", (e) =>{
-    endTrivia()
-    nextButton.style.visibility = "hidden"
-    comments.innerText = " "
-    container.innerText = allQuestions[currentRound].question
-    optionsContainer[0].innerText = allQuestions[currentRound].options[0]
-    optionsContainer[1].innerText = allQuestions[currentRound].options[1]
-    optionsContainer[2].innerText = allQuestions[currentRound].options[2]
-    optionsContainer[3].innerText = allQuestions[currentRound].options[3]
-    console.log(currentRound)
-    })
-    
 
-function endTrivia(){
-    if(currentRound === allQuestions.length){
+
+function endTrivia() {
+    if (currentRound === allQuestions.length) {
         container.innerText = `ALL DONE! You scored ${points} out of ${allQuestions.length}`
+        // try to do this with a loop or .forEach()
         optionsContainer[0].innerText = " "
         optionsContainer[1].innerText = " "
         optionsContainer[2].innerText = " "
         optionsContainer[3].innerText = " "
         comments.innerText = " "
-        // tryAgain.style.visibility =  "visible"
-        // nextButton.style.visibility = "hidden"
+        tryAgain.style.visibility = "visible"
+        nextButton.style.visibility = "hidden"
     }
 }
 
-tryAgain.addEventListener("click", (e)=>{
-    let currentRound = 0;
-    container.innerText = allQuestions[currentRound].question
-    optionsContainer[0].innerText = allQuestions[currentRound].options[0]
-    optionsContainer[1].innerText = allQuestions[currentRound].options[1]
-    optionsContainer[2].innerText = allQuestions[currentRound].options[2]
-    optionsContainer[3].innerText = allQuestions[currentRound].options[3]
-})
+function goAgain() {
+    tryAgain.addEventListener("click", (e) => {
+        window.location.reload();
+    })
+}
+
+startGame()
+chooseAnswer()
+hitNext()
+endTrivia()
+goAgain()
